d91e3c9825e6fcef05f88355bbcc378ae4f17f05
aby <aby@luckru.ru>
2023-03-14 08:00:38 +0500

    sessions refactoring

--- app/Middleware/ValidationErrorsMiddleware.php
+++ app/Middleware/ValidationErrorsMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Middleware;
 
+use App\Contracts\SessionInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -10,17 +11,16 @@ use Slim\Views\Twig;
 
 class ValidationErrorsMiddleware implements MiddlewareInterface
 {
-    public function __construct( private readonly Twig $twig )
-    {
+    public function __construct(
+        private readonly Twig $twig,
+        private readonly SessionInterface $session
+    ) {
     }
 
     public function process( ServerRequestInterface $request, RequestHandlerInterface $handler ): ResponseInterface
     {
-        if ( !empty( $_SESSION[ 'errors' ] ) ) {
-            $errors = $_SESSION[ 'errors' ];
+        if ( $errors = $this->session->getFlash( 'errors' ) ) {
             $this->twig->getEnvironment()->addGlobal( 'errors', $errors );
-
-            unset( $_SESSION[ 'errors' ] );
         }
 
         return $handler->handle( $request );
